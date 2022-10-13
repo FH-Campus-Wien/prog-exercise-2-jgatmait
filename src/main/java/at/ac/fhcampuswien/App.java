9f44f37403c0e6b573a278fdc6bc1fb59b07bd8b
Janella Gatmaitan <janella.gatmaitan@gmail.com>
2022-10-13 13:03:38 +0200

    rhombus shape ok

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,34 +4,149 @@ import java.util.Scanner;
 
 public class App {
 
+    Scanner scanner = new Scanner(System.in);
     //todo Task 1
     public void largestNumber(){
         // input your solution here
+        // case 1: bis Input 0
+        // do while
+        double number, largest;
+        int x = 1;
+        largest = 1;
+
+
+
+       /* do {
+            System.out.print("Number " + x + ": ");
+            number = scanner.nextDouble();
+
+            if( (number <= 0) && (x==1)){
+                System.out.print("No number entered.");
+                x++;
+            }else if ((number <=0) && (x>1)) {
+                System.out.print("The largest number is ");
+                System.out.printf("%.2f", largest);
+                System.out.println();
+                x++;
+            }else if (number > largest) {
+                largest = number;
+                x++;
+            }else
+                x++;
+        }while (number != 0) ;*/
+
+
     }
 
     //todo Task 2
     public void stairs(){
         // input your solution here
+        // keine Arrays verwenden
+        //debugger benutzen
+        //Schleife verschachteln
+        /*int n, counter;
+        int rows;
+        int cols;
+        counter = 1;
+        System.out.print("n: ");
+        n = scanner.nextInt();
+
+
+        for (rows = 0; rows < n; rows++){
+            for (cols = 0; cols <= rows; cols++, counter++ ){
+                System.out.print(counter + " ");
+            }System.out.println("");
+        }*/
+
+
+
+
     }
 
     //todo Task 3
     public void printPyramid(){
         // input your solution here
+       /* int rows = 6;
+
+
+
+
+        for (int z = 0; z < rows; z++ ){
+            for (int spaces = rows - 1; spaces > z ; spaces--){
+                System.out.print(" ");
+            }for (int stars = 0; stars <=z*2; stars++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+
+        }*/
+
+
     }
 
     //todo Task 4
     public void printRhombus(){
         // input your solution here
+        int h;
+        char c;
+
+        System.out.print("h: ");
+        h = scanner.nextInt();
+        System.out.print("c: ");
+        c = scanner.next().charAt(0); //stackoverflow.com
+        int letter = c ;
+        int count = 0;
+        int number = 0;
+
+
+
+        for ( int i = 1; i <= h ; i++){
+
+            if ( i <= h/2 + 1){
+                count = 2 * i - 1;
+                number = i;
+            }else{
+                count = count - 2;
+                number = i;
+            }
+            for( int j = 1; j <= count ; j++){
+                System.out.print(number);
+            }
+
+
+
+
+
+            System.out.println();
+        }
+
+
+
+
+
+
+
     }
 
     //todo Task 5
     public void marks(){
         // input your solution here
+        //
     }
 
     //todo Task 6
     public void happyNumbers(){
         // input your solution here
+        //aufhören bei 1 oder 4
+        int n;
+        System.out.print("n: ");
+        n = scanner.nextInt();
+
+
+
+
     }
 
     public static void main(String[] args){
